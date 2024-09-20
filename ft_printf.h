96a3c949c66f9b7402624c578286f7db685c6011
ori <142668608+oridamasceno@users.noreply.github.com>
2024-09-20 11:32:37 -0300

    Update ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -1,14 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ledamasc <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/17 18:54:19 by ledamasc          #+#    #+#             */
-/*   Updated: 2023/12/18 20:21:35 by oridamasceno     ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
@@ -17,11 +6,11 @@
 # include <stddef.h>
 # include <stdlib.h>
 
-void			ft_putchar_len(char character, int *len);
+void			ft_putlen(char character, int *len);
 void			ft_string(char *args, int *len);
 void			ft_number(int number, int *len);
-void			ft_hexadecimal(unsigned int x, int *len, char x_or_x);
-void			ft_unsigned_int(unsigned int u, int *len);
+void			ft_hexa(unsigned int x, int *len, char x_or_x);
+void			ft_unsigned(unsigned int u, int *len);
 void			ft_pointer(size_t pointer, int *len);
 
 int				ft_printf(const char *string, ...);
