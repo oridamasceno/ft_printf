517d71706d6a8466184e5aadbbb240ff5cfbbac1
ori <142668608+oridamasceno@users.noreply.github.com>
2024-09-20 11:27:43 -0300

    Update ft_letters.c

--- ft_letters.c
+++ ft_letters.c
@@ -1,19 +1,8 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_letters.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ledamasc <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/19 19:26:13 by ledamasc          #+#    #+#             */
-/*   Updated: 2023/12/19 19:37:19 by ledamasc         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 #include "ft_printf.h"
 
-void	ft_putchar_len(char character, int *len)
+void	ft_putlen(char c, int *len)
 {
-	write(1, &character, 1);
+	write(1, &c, 1);
 	(*len)++;
 }
 
@@ -30,7 +19,7 @@ void	ft_string(char *args, int *len)
 	}
 	while (args[i] != '\0')
 	{
-		ft_putchar_len(args[i], len);
+		ft_putlen(args[i], len);
 		i++;
 	}
 }
